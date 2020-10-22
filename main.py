195c712e7159c4520c2def8b3d7a4b18770bd53d
Zait06 <zait-hdez@hotmail.com>
2020-10-21 22:39:03 -0500

    Main program debug

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from utilities import colored
 
 if __name__ == "__main__":
 
-    dnatk = DNAToolkit()
+    randStr = createRandDNA(50)
 
-    randStr = dnatk.createRandDNA(50)
-
-    DNAStr = dnatk.validateSeq(randStr)
+    DNAStr = validateSeq(randStr)
 
     print(f"\nSequence: {colored(DNAStr)}")
     print(f"[1] + Sequence length: {len(DNAStr)}")
-    print(colored(f"[2] + Nucleotide Frequency {dnatk.countNucFrequency(DNAStr)}"))
-    print(f"[3] + DNA/RNA Transcription: {colored(dnatk.transcription(DNAStr))}")
+    print(colored(f"[2] + Nucleotide Frequency {countNucFrequency(DNAStr)}"))
+    print(f"[3] + DNA/RNA Transcription: {colored(transcription(DNAStr))}")
     print(f"[4] + DNA String + Reverese Complemet:\n")
     print(f"3\' {colored(DNAStr)} 5\'")
     print(f"   "+''.join(['|' for _ in range(len(DNAStr))]))
-    print(f"3\' {colored(dnatk.reverseComplement(DNAStr))} 5\'\n")
+    print(f"3\' {colored(reverseComplement(DNAStr))} 5\'\n")
